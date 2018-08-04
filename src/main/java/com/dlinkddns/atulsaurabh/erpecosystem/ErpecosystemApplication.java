d468ccf5630350f1ad3f9b15e0794860bbd93761
Atul Saurabh <atul.saurabh@gmail.com>
2018-08-04 07:29:44 +0530

    Project Structure Created And Dependency Managed

---
+++ src/main/java/com/dlinkddns/atulsaurabh/erpecosystem/ErpecosystemApplication.java
@@ -0,0 +1,12 @@
+package com.dlinkddns.atulsaurabh.erpecosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpecosystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpecosystemApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dlinkddns/atulsaurabh/erpecosystem/ErpecosystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dlinkddns.atulsaurabh.erpecosystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ErpecosystemApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
